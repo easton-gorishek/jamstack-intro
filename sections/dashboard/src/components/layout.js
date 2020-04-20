873bcd05cd2e3a63b7bf2ba9fae823724d0a280a
Easton Gorishek <ieaston26@gmail.com>
2020-04-19 21:48:11 -0700

    add private routes

--- sections/dashboard/src/components/layout.js
+++ sections/dashboard/src/components/layout.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "gatsby";
+import { IdentityContextProvider } from "react-netlify-identity-widget";
 
 import "./layout.css";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <IdentityContextProvider url="https://hungry-meitner-6fb34a.netlify.app">
       <header>
         <Link to="/">JAMStack App</Link>
       </header>
       <main>{children}</main>
-    </>
+    </IdentityContextProvider>
   );
 };
-
 
 export default Layout;
