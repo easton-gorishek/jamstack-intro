42ed89e1917128ff26d09a6a8ffeed87cf08b4b7
Easton Gorishek <ieaston26@gmail.com>
2020-04-21 13:50:22 -0700

    render todo list

---
+++ sections/todos/src/pages/index.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "./index.module.css";
+import Todo from "../components/todo";
+
+export default () => {
+  const [status, setStatus] = useState("loading");
+  const [todos, setTodos] = useState(null);
+
+  useEffect(() => {
+    let canceled = false;
+    if (status !== "loading") return;
+
+    axios("/api/get-all-todos").then((res) => {
+      if (canceled === true) return;
+
+      if (res.status !== 200) {
+        console.error("Error loading todos!");
+        console.log(res);
+        return;
+      }
+
+      setTodos(res.data.todos);
+      setStatus("loaded");
+    });
+
+    return () => {
+      canceled = true;
+    };
+  }, [status]);
+  return (
+    <main>
+      <h1 className={styles.heading}>JAMStack Todos!</h1>
+      {todos ? (
+        <ul className={styles.todos}>
+          {todos.map((todo) => (
+            <li key={todo._id} className={styles.todo}>
+              <Todo todo={todo} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className={styles.loading}>loading...</p>
+      )}
+    </main>
+  );
+  // TODO add todos
+};
